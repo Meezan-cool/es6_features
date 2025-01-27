3f0cccb45c89109dfd6fa1baf6820feb1aeea9fb
Meezan-onepiece <135635540+Meezan-cool@users.noreply.github.com>
2025-01-27 17:48:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//  var can be redeclared / global & locally hoisted 
+var userName = 'Meezan'
+
+//  let can't be redeclared but can be reassigned / block hoisted
+let age;
+age = 20
+console.log(age)
+
+//  let can't be redeclared but can be reassigned / block hoisted
+const address = 'India';
+console.log(address)
