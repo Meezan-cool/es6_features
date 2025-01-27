d61fe21858cfc94df37118a991684486fc1ad553
Meezan-onepiece <135635540+Meezan-cool@users.noreply.github.com>
2025-01-27 19:20:50 +0530

    added the content of lecture as explained

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-'use strict';
+'use strict';// we can use use strict in every block first line to use it 
+// to make some strict process like declaration of variable used keyword like 
+// var , let , const not like this 'userName = Meezan'
 
 //  var can be redeclared / global & locally hoisted 
 var userName = 'Meezan'
@@ -11,3 +13,14 @@ console.log(age)
 //  let can't be redeclared but can be reassigned / block hoisted
 const address = 'India';
 console.log(address)
+
+const arr = ['hobbies'];
+arr.push('gaming');
+// Output -> ['hobbies' , 'gaming' ] even it's a constant but we can change it's value 
+// due to preference data type but we cannot reassign them.
+
+const arr1 = arr;
+arr.push('development');
+// arr = ['hobbies' , 'gaming' , 'development']
+// arr1 = ['hobbies' , 'gaming' , 'development'] it's same as arr because it's 
+// copies a reference to the arr and will have the same value as arr
